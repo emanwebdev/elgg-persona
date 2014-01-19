c695e83d11ca9533416d116c4c39572723952ed1
Emmanuel еΜanwʬĔbdƎv <emanwebdev@gmail.com>
2014-01-19 12:12:01 +0100

    Update fr.php: TYPOS + new string

--- persona/languages/fr.php
+++ persona/languages/fr.php
@@ -10,24 +10,24 @@
 	    
 	    'persona:details'	=>  'Vous y êtes presque!',
 	    'persona:details:explanation'	=>  
-				    "Pour commencer, nous avons besoin que vous nous donniez quelques informations à votre sujet. Elles vous aideront obtenir le maximum du site.",
+				    "Pour commencer, nous avons besoin que vous nous donniez quelques informations à votre sujet. Elles vous aideront à obtenir le maximum du site.",
 	    
 	    'persona:username'	=>  'Votre identifiant d\'utilisateur',
 	    'persona:username:explanation'
-				=>  'C\'est l\'identifiant que vous utilisez sur le site. Il doit être d\'au moins quatre caractères, sans espaces, ni signes de ponctuation ou caractères spéciaux. Si p.ex. votre nom est Jean Dupont, vous pouvez choisir JeanDupont. Une fois que vous l\'avez défini, il ne pourra plus être changé.',
+				=>  'C\'est l\'identifiant que vous utiliserez sur le site. Il doit comporter au moins 4 caractères, sans espaces, ni signes de ponctuation ou caractères spéciaux. Si p.ex. votre nom est Jean Dupont, vous pouvez choisir JeanDupont. Une fois que vous l\'aurez défini, il ne pourra plus être changé.',
 	
 	    'persona:name'	=>  'Votre nom',
 	    'persona:name:explanation'
-				=>  'C\'est le nom qui sera rattaché à votre profil, sur tout contenu que vous publiez, et sur l\'ensemble de vos commentaires. Vous pouvez le modifier ultérieurement.',
+				=>  'C\'est le nom qui sera associé à votre profil, au contenu que vous publiez et sur l\'ensemble de vos commentaires. Vous pouvez le modifier ultérieurement.',
 	    
 	    'persona:settings:instructions' =>
-				    'Persona est une norme d\'authentification facile à utiliser créé par Mozilla. Ce plugin permet à vos utilisateurs d\'optionnellement utiliser Persona pour se connecter à votre site Elgg. Tout ce qu\'il ont besoin est une adresse e-mail - c\'est sans doute plus facile que l\'authentification intégrée de Elgg',
+				    'Persona est une norme d\'authentification facile à utiliser créé par Mozilla. Ce plugin permet à vos utilisateurs d\'optionnellement utiliser Persona pour se connecter à votre site Elgg. Tout ce qu\'ils ont besoin est une adresse e-mail - c\'est sans doute plus facile que l\'authentification intégrée de Elgg',
 	    'persona:login:use'	=>  'Activer les connexions Persona sur la page d\'accueil?',
 	    
 	    'username:invalid'	=>  'Le nom d\'utilisateur est invalide. Veuillez svp en entrer un autre.',
 	    'name:invalid'	=>  'Vous devez entrer un nom!',
 	    'persona:registered'
-				=>  "You're ready to go! Why not fill in your profile to tell other users a little about you?",
+				=>  "Vous êtes prêt à commencer! Pourquoi ne pas compléter votre profil? Pour vous présenter aux autres utilisateurs! Parlez-leur un peu de vous!",
 		
 	);
 
